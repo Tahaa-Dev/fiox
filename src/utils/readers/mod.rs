ce40ef49af42f9dbbec8f830c354dcd3eaaf304c
tahamahmoud7097-wq <tahamahmoud7097@gmail.com>
2025-12-18 15:16:13 +0200

    Added NDJSON reader to add full NDJSON support in fiox.

--- src/utils/readers/mod.rs
+++ src/utils/readers/mod.rs
@@ -1,3 +1,4 @@
 pub mod csv_reader;
 pub mod json_reader;
+pub mod ndjson_reader;
 pub mod toml_reader;
---
+++ src/utils/readers/ndjson_reader.rs
@@ -0,0 +1,16 @@
+use std::{fs::File, io::BufReader, path::PathBuf};
+
+use crate::utils::BetterExpect;
+
+pub fn ndjson_reader(path: &PathBuf, verbose: bool) -> BufReader<File> {
+    let file = File::open(path).better_expect(
+        format!(
+            "ERROR: Failed to open input file [{}] for reading.",
+            path.to_str().unwrap_or("[input.ndjson]")
+        )
+        .as_str(),
+        verbose,
+    );
+
+    BufReader::with_capacity(256 * 1024, file)
+}
